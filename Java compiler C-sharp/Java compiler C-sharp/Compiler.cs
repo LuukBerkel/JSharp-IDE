640399227650a99ffd0dc171b44f52fb87716cba
luuki <lah.vanberkel@student.avans.nl>
2021-09-25 21:52:18 +0200

    Added testproject for compiler

---
+++ Java compiler C-sharp/Java compiler C-sharp/Compiler.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Java_compiler_C_sharp
+{
+    class Compiler
+    {
+        public string CompilerPath { get; set; }
+
+        public Compiler(string compilerPath)
+        {
+            CompilerPath = compilerPath;
+        }
+
+
+        public void Compile(string path, string file)
+        {
+
+            using (var compileTask = new Process())
+            {
+                //Checks
+                if (!Directory.Exists(CompilerPath)) throw new Exception("Invalid java directory");
+                if (!File.Exists(path + @"\" + file)) throw new Exception("Invalid file path");
+
+
+
+                //Arguments
+                string compileCommand = $"javac {file}";
+
+                //Process
+                compileTask.StartInfo.FileName = @"cmd.exe";
+                compileTask.StartInfo.WorkingDirectory = path;
+                compileTask.StartInfo.EnvironmentVariables["Path"] = this.CompilerPath;
+                compileTask.StartInfo.RedirectStandardInput = true;
+                compileTask.StartInfo.RedirectStandardOutput = true;
+                compileTask.StartInfo.RedirectStandardError = true;
+                compileTask.Start();
+
+
+
+                //Execution
+                compileTask.StandardInput.WriteLine(compileCommand);
+
+
+                //TODO catching the error
+                //Checking
+                while (true)
+                {
+                    string error = compileTask.StandardError.ReadLine();
+                    if (error == null)
+                    {
+                        break;
+                    }
+
+                }
+               
+
+                
+                
+
+            }
+        }
+    }
+
+   
+
+
+   
+}
---
+++ Java compiler C-sharp/Java compiler C-sharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Java_compiler_C_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Compiler compiler = new Compiler(@"C:\Program Files\Java\jdk1.8.0_261\bin");
+
+            try
+            {
+                compiler.Compile(@"C:\Users\berke\Downloads\", "helloworld.java");
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}

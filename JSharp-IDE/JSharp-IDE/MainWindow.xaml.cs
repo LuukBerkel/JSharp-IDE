8cb54a3f83bedff0ffc17d4ed26a1c401464f72f
Lars <larslego@outlook.com>
2021-09-28 11:00:17 +0200

    Added: Basic UI

--- JSharp-IDE/JSharp-IDE/MainWindow.xaml.cs
+++ JSharp-IDE/JSharp-IDE/MainWindow.xaml.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace JSharp_IDE
+namespace IDE_testing
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -22,7 +16,48 @@ namespace JSharp_IDE
     {
         public MainWindow()
         {
-            InitializeComponent();
+            
+        }
+
+        private void CodeTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            RichTextBox rtb = sender as RichTextBox;
+            //Remove this event to prevent a recursive call
+            rtb.TextChanged -= CodeTextBox_TextChanged;
+            int result = 0;
+            Task.Run(async () =>
+            {
+                result = await TextFormatter.OnTextChanged(rtb);
+                //Update after the previous update was finished.
+                if (result == 1)
+                {
+                    //Add this event back when the operation is finished.
+                    rtb.TextChanged += CodeTextBox_TextChanged;
+                }
+            });
+        }
+
+        private void CodeTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            RichTextBox rtb = sender as RichTextBox;
+            switch (e.Key)
+            {
+                case Key.Tab:
+                    rtb.CaretPosition.InsertTextInRun("    ");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void CodeTextBox_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            RichTextBox rtb = sender as RichTextBox;
+            Task.Run(async () =>
+            {
+                Debug.WriteLine("CodeTextBox_Pasting");
+                await TextFormatter.OnTextPasted(rtb);
+            });
         }
     }
 }
---
+++ JSharp-IDE/JSharp-IDE/TextFormatter.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace IDE_testing
+{
+    class TextFormatter
+    {
+        private static SolidColorBrush standardColor = Brushes.White;
+
+        //Keywords
+        private static SolidColorBrush keyWordColor = Brushes.Orange;
+        private static string keywords = @"\b(public|private|class|void|import|protected|static|final|enum|synchronized|super|this|boolean|while|for|;|true|false|case|break|if|switch|else|int|new|return|try|catch|finally|implements|extends)\b\s*";
+
+        //Annotations
+        private static SolidColorBrush annotationColor = Brushes.YellowGreen;
+        private static string annotations = @"@[a-zA-Z]+";
+        
+        //Comments
+        private static SolidColorBrush commentColor = Brushes.Green;
+        private static string comments = @"(\/\*\*)(.|\n)+?(\*\/)";
+
+        public static async Task<int> OnTextChanged(RichTextBox rtb)
+        {
+            rtb.Dispatcher.Invoke(() => {
+                TextPointer caretPos = rtb.CaretPosition;
+                //Find the current block (line in this case) where the caret (typing cursor) is at.
+                Block block = rtb.Document.Blocks.Where(x => x.ContentStart.CompareTo(caretPos) == -1 && x.ContentEnd.CompareTo(caretPos) == 1).FirstOrDefault();
+                ChangeSelectedTextColor(new TextRange(block.ContentStart, block.ContentEnd), standardColor);
+                CheckSyntaxAtBlock(block);
+            });
+
+            return 1;
+        }
+
+        public static async Task<int> OnTextPasted(RichTextBox rtb)
+        {
+            //Filter on all the defined words and give them the correct color.
+            rtb.Dispatcher.Invoke(() => {
+                BlockCollection blocks = rtb.Document.Blocks;
+                if (blocks != null)
+                {
+                    for (int i = 0; i < blocks.Count; i++)
+                    {
+                        // Check if the user is typing within this block.
+                        var start = blocks.ElementAt(i).ContentStart;
+                        TextRange range = new TextRange(blocks.ElementAt(i).ContentStart, blocks.ElementAt(i).ContentEnd.GetPositionAtOffset(-1));
+                        ChangeSelectedTextColor(range, standardColor);
+                        CheckSyntaxAtBlock(blocks.ElementAt(i));
+                    }
+                }
+            });
+            return 1;
+        }
+
+        private static void CheckSyntaxAtBlock(Block block)
+        {
+            if (block != null)
+            {
+                var start = block.ContentStart;
+                while (start != null && start.CompareTo(block.ContentEnd) < 0)
+                {
+                    if (start.GetPointerContext(LogicalDirection.Forward) == TextPointerContext.Text)
+                    {
+                        MatchRegexAndHighlight(start, new Regex(keywords), keyWordColor);
+                        MatchRegexAndHighlight(start, new Regex(annotations, RegexOptions.Compiled | RegexOptions.IgnoreCase), annotationColor);
+                        start = MatchRegexAndHighlight(start, new Regex(comments, RegexOptions.Compiled | RegexOptions.IgnoreCase), commentColor);
+                    }
+                    start = start.GetNextContextPosition(LogicalDirection.Forward);
+                }
+            } else
+            {
+                Debug.WriteLine("CheckSyntaxAtBlock: block == null");
+            }
+        }
+
+        private static TextPointer MatchRegexAndHighlight(TextPointer start, Regex rex, SolidColorBrush color)
+        {
+            Match match = rex.Match(start.GetTextInRun(LogicalDirection.Forward));
+            TextRange textRange = new TextRange(start.GetPositionAtOffset(match.Index, LogicalDirection.Forward), start.GetPositionAtOffset(match.Index + match.Length, LogicalDirection.Backward));
+            ChangeSelectedTextColor(textRange, color);
+            return textRange.End;
+        }
+
+        private static void ChangeSelectedTextColor(TextRange textRange, SolidColorBrush color)
+        {
+            textRange.ApplyPropertyValue(TextElement.ForegroundProperty, color);
+        }
+    }
+}

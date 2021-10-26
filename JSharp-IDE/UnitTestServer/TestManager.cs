e0d9634777a8d32e42791a75d51ec1699fa26954
Lars <larslego@outlook.com>
2021-10-26 13:25:08 +0200

    Edit: Implemented the final unit tests (20.9%)

---
+++ JSharp-IDE/UnitTestServer/TestManager.cs
@@ -0,0 +1,69 @@
+﻿using JSharp_Server.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestServer
+{
+    [TestClass]
+    public class TestManager
+    {
+        [TestMethod]
+        public void Test_CheckUser_Success()
+        {
+            // Arrange
+            Manager manager = new Manager();
+            manager.AddUser("Gebruiker", "wachtwoord");
+
+            // Act
+            User result = manager.CheckUser("Gebruiker", "wachtwoord");
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Test_CheckUser_Fail()
+        {
+            // Arrange
+            Manager manager = new Manager();
+            manager.AddUser("Gebruiker", "wachtwoord");
+
+            // Act
+            User result = manager.CheckUser("", "wachtwoord");
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void Test_AddUser_Success()
+        {
+            // Arrange
+            Manager manager = new Manager();
+
+            // Act
+            bool result = manager.AddUser("xX_Programmer_Xx", "wachtwoord");
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_AddUser_Fail()
+        {
+            // Arrange
+            Manager manager = new Manager();
+            manager.AddUser("Gebruiker", "wachtwoord");
+
+            // Act
+            bool result = manager.AddUser("Gebruiker", "wachtwoord");
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
